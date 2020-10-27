879b24e04722f6c8dd66e81919d2c1bccfbb3a72
Bipolairbeer <47387168+Bipolairbeer@users.noreply.github.com>
2020-10-27 10:01:10 -0400

    qbuild.sh

--- qbuild.sh
+++ qbuild.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
+#################################################################################################################
+#                                          Build Qubes in Fedora 32
+#
+# Open a terminal 
+# --> sudo dnf update && sudo dnf upgrade
+# --> sudo git clone git://github.com/Bipolairbeer/q-builder /home/user/q-builder
+# --> sudo chmod +x /home/user/q-builder/qbuild.sh
+# --> cd q-builder/
+# --> sudo ./qbuild.sh
+#################################################################################################################
+
 yes | sudo dnf install -y wget make git qubes-gpg-split createrepo rpm-build rpm-sign make python3-sh rpmdevtools rpm-sign dialog perl-open python3-pyyaml perl-Digest-MD5 perl-Digest-SHA 
 
-sudo git clone "https://github.com/QubesOS/qubes-secpack.git"
+sudo git clone "https://github.com/QubesOS/qubes-secpack.git" /home/user/qubes-secpack
 gpg --import qubes-secpack/keys/*/*
 cat<<-EOF|gpg --command-fd 0 --edit-key 36879494
 	fpr
@@ -14,12 +26,12 @@ cat<<-EOF|gpg --command-fd 0 --edit-key 36879494
 cd qubes-secpack/
 git tag -v `git describe`
 
-cd canaries/ 
+cd canaries/
 gpg --verify canary-001-2015.txt.sig.joanna canary-001-2015.txt
 gpg --verify canary-001-2015.txt.sig.marmarek canary-001-2015.txt
 
 cd
-sudo git clone "https://github.com/QubesOS/qubes-builder.git"
+sudo git clone "https://github.com/QubesOS/qubes-builder.git" /home/user/qubes-builder
 
 cd qubes-builder/
 git tag -v `git describe`
@@ -29,24 +41,20 @@ gpg --keyserver pgp.mit.edu --recv-keys 916B8D99C38EAF5E8ADC7A2A8D66066A2EEACCDA
 gpg --fingerprint 916B8D99C38EAF5E8ADC7A2A8D66066A2EEACCDA
 export GNUPGHOME=~/qubes-builder/keyrings/git
 mkdir --parents "$GNUPGHOME"
-cp ~/.gnupg/pubring.gpg "$GNUPGHOME"
+cp ~/.gnupg/pubring.kbx "$GNUPGHOME"
 cp ~/.gnupg/trustdb.gpg "$GNUPGHOME"
 chmod --recursive 700 "$GNUPGHOME"
 
-sudo git clone "https://github.com/Bipolairbeer/qubes-builder.git" /home/user/bipolairbeer
+cd
+sudo git clone "https://github.com/Bipolairbeer/q-builder.git" /home/user/q-builder
 
 cd
-cp /home/user/bipolairbeer/builder.conf /home/user/qubes-builder/builder.conf
+sudo rm rf /home/user/qubes-builder/builder.conf
+sudo cp -r /home/user/q-builder/builder.conf /home/user/qubes-builder/builder.conf
 
-cd /home/user/qubes-builder
+cd qubes-builder/
 sudo make install-deps
 sudo make get-sources
 sudo make qubes
 sudo make sign-all
 sudo make iso
-
-
-
- 
-
-
