278ca1d26f29498fd48f1dbb66f76c7157255d2a
Bipolairbeer <47387168+Bipolairbeer@users.noreply.github.com>
2020-10-29 07:12:42 -0400

    qbuild.sh

--- qbuild.sh
+++ qbuild.sh
@@ -14,8 +14,17 @@
 yes | sudo dnf install -y wget make git qubes-gpg-split createrepo rpm-build rpm-sign make python3-sh rpmdevtools rpm-sign dialog perl-open python3-pyyaml perl-Digest-MD5 perl-Digest-SHA 
 
 cd
+sudo git clone git://github.com/QubesOS/qubes-builder.git qubes-builder 
+
+cd qubes-builder
+sudo git tag -v `git describe`
+
+cd
 sudo git clone git://github.com/QubesOS/qubes-secpack.git qubes-secpack
+
+cd
 gpg --import qubes-secpack/keys/*/*
+
 cat<<-EOF|gpg --command-fd 0 --edit-key 36879494
 	fpr
 	trust
@@ -26,16 +35,6 @@ cat<<-EOF|gpg --command-fd 0 --edit-key 36879494
 
 cd qubes-secpack
 git tag -v `git describe`
-
-cd canaries/
-gpg --verify canary-001-2015.txt.sig.joanna canary-001-2015.txt
-gpg --verify canary-001-2015.txt.sig.marmarek canary-001-2015.txt
-
-cd
-sudo git clone git://github.com/QubesOS/qubes-builder.git qubes-builder 
-
-cd qubes-builder
-sudo git tag -v `git describe`
 
 cd
 sudo rm -rf /home/user/qubes-builder/builder.conf
@@ -53,7 +52,6 @@ chmod --recursive 700 "$GNUPGHOME"
 cd qubes-builder
 sudo make get-sources
 sudo make vmm-xen core-admin linux-kernel gui-daemon template desktop-linux-kde installer-qubes-os manager linux-dom0-updates
-
 sudo make install-deps
 sudo make qubes
 sudo make sign-all
